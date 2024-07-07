b09a91c4f4476cecb2ea3b22686bbe6b3bb53e3c
Abhinay Ambati <151802585+AbhinayAmbati@users.noreply.github.com>
2024-07-07 21:13:18 +0530

    Update Player.jsx

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -20,12 +20,9 @@ const Player = () => {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: '
-        //add api key
-        '
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMzJmMGEwMzUzZTk5YzI2ODdkNzMzMmM3NjgyNjE1OCIsInN1YiI6IjY2NzMwZjViNDliODgzNWMwNzU5ZTBkMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.E9pur3jhU-227BFZHQp7KNvUI-khUWuKmwm--298Mqc'
     }
   };
-
   useEffect(() => {
     fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
       .then(response => response.json())
